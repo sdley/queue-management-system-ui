9ba4581b14703db02374c076d07895b435aca668
Souleymane DIALLO <sdley2020@gmail.com>
2025-04-03 18:13:17 +0000

    project structure [ui]

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
-    <div>My Queue Management System Home page</div>
+    <Navbar />
   );
 }
