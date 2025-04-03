901590be774ae2308ed0adddbe831db56b9b8319
Souleymane DIALLO <sdley2020@gmail.com>
2025-04-03 20:10:23 +0000

    Fixed NavBar

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
