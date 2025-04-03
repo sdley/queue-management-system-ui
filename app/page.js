ce427db71323e13310a4d4345669a62432486edb
Souleymane DIALLO <sdley2020@gmail.com>
2025-04-03 20:24:34 +0000

    Updated Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold text-[#b26148] mb-6"
-        >Bienvenue sur l&apos;application de gestion des agents</h1>
-      <p className="text-lg text-gray-700">Sélectionnez une option dans le menu pour commencer.</p>
-    </div>
+      <div className="flex flex-col items-center justify-center mt-10">
+          <h1 className="text-4xl font-bold text-[#b26148] mb-6">
+              Bienvenue sur l&apos;application de gestion de file d&apos;attente !
+          </h1>
+          <p className="text-lg text-gray-700">
+              Sélectionnez une option dans le menu pour commencer.
+          </p>
+      </div>
   );
 }
